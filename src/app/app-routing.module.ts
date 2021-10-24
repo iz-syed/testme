61f13085a3d62c2e3c8efe0b446330b92390e9d0
Mubarak Cc7 <iz-syed@users.noreply.github.com>
2021-10-24 23:37:58 +0530

    Route + Module + Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,35 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BlankComponent } from './layouts/blank/blank.component';
+import { FullComponent } from './layouts/full/full.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'auth',
+    component:BlankComponent,
+    children:[
+      {
+        path:'',
+        loadChildren: ()=> import('./modules/auth/auth.module').then( m => m.AuthModule)
+      }
+    ]
+  },
+  {
+    path:'',
+    component:FullComponent,
+    children:[
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path:'',
+        loadChildren: ()=> import('./modules/dashboard/dashboard.module').then( m => m.DashboardModule)
+      }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlankComponent } from './layouts/blank/blank.component';
+import { FullComponent } from './layouts/full/full.component';
+import { LoginComponent } from './modules/login/login.component';
+import { HeaderComponent } from './components/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlankComponent,
+    FullComponent,
+    LoginComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
